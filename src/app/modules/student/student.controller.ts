8b670fe7bf9af49442de8189018a6bedc2312032
Chishty313 <chishty828rahman@gmail.com>
2024-05-21 08:19:10 +0600

    Done with this project

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    //   Will call service function to send this data
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    // Send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students data retrived successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentID } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentID);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
