f6f419555a5078ed961164ab8128516b0da4f15e
Chishty313 <chishty828rahman@gmail.com>
2024-06-08 04:05:35 +0600

    Completed till creating academic semester interface

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -38,7 +38,7 @@ export const createStudentValidationSchema = z.object({
     student: z.object({
       name: UserNameValidationSchema,
       gender: z.enum(['male', 'female', 'other']),
-      dateOfBirth: z.string(),
+      dateOfBirth: z.date().optional(),
       email: z.string().email(),
       contactNo: z.string(),
       emergencyContactNo: z.string(),
