775c89368373a391815442f951859577c404ae42
Chishty313 <chishty828rahman@gmail.com>
2024-06-08 02:20:35 +0600

    Completed till implementing middleware army

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -2,12 +2,7 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
-
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-  };
-};
+import catchAsync from '../../utils/catchAsync';
 
 const getAllStudents = catchAsync(async (req, res, next) => {
   const result = await StudentServices.getAllStudentsFromDB();
@@ -20,7 +15,7 @@ const getAllStudents = catchAsync(async (req, res, next) => {
   });
 });
 
-const getSingleStudent = catchAsync(async (req, res, next) => {
+const getSingleStudent = catchAsync(async (req, res) => {
   const { studentID } = req.params;
   const result = await StudentServices.getSingleStudentFromDB(studentID);
 
@@ -32,7 +27,7 @@ const getSingleStudent = catchAsync(async (req, res, next) => {
   });
 });
 
-const deleteStudent = catchAsync(async (req, res, next) => {
+const deleteStudent = catchAsync(async (req, res) => {
   const { studentID } = req.params;
   const result = await StudentServices.deleteStudentFromDB(studentID);
 
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,34 +2,28 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    // Creating a schema validation using zod
-    const { password, student: studentData } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+  // Creating a schema validation using zod
+  const { password, student: studentData } = req.body;
 
-    // const zodParsedData = StudentValidationSchema.parse(studentData);
+  // const zodParsedData = StudentValidationSchema.parse(studentData);
 
-    // // data validation using joi
-    // const { error, value } = StudentValidationSchema.validate(studentData);
+  // // data validation using joi
+  // const { error, value } = StudentValidationSchema.validate(studentData);
 
-    //   Will call service function to send this data
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
+  //   Will call service function to send this data
+  const result = await UserServices.createStudentIntoDB(password, studentData);
 
-    // Send response
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student created successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  // Send response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
