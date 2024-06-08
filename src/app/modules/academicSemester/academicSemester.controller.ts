f94d507955d02564bbe50ef42438cd0460485920
Chishty313 <chishty828rahman@gmail.com>
2024-06-08 06:50:38 +0600

    Completed till creating academic semester validation, route and controller

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,10 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import router from '../../routes';
+
+const createAcademicSemester = catchAsync(async (req, res) => {});
+
+export const AcademicSemesterControllers = {
+  createAcademicSemester,
+};
