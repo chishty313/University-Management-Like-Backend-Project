908b903fda5e9691624b8c9d448bd67eb0de1eaa
Chishty313 <chishty828rahman@gmail.com>
2024-06-09 12:37:39 +0600

    Completed till handling logical validation of academic semester

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -36,6 +36,17 @@ const AcademicSemesterSchema = new Schema<TAcademicSemester>(
   { timestamps: true },
 );
 
+AcademicSemesterSchema.pre('save', async function (next) {
+  const isSemesterExists = await AcademicSemester.findOne({
+    year: this.year,
+    name: this.name,
+  });
+  if (isSemesterExists) {
+    throw new Error('Semester already exists !!');
+  }
+  next();
+});
+
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
   AcademicSemesterSchema,
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,18 @@
-import { TAcademicSemester } from './academicSemester.interface';
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
+import {
+  TAcademicSemester,
+  TAcademicSemesterNameCodeMapper,
+} from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // semester name --> semester code
+  if (
+    academicSemesterNameCodeMapper[payload.name] !==
+    academicSemesterNameCodeMapper[payload.code]
+  ) {
+    throw new Error('Invalid semester code !!');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
