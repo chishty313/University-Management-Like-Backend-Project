a3df563b0f4e33c60462bc2f0bfad848444ec9d2
Chishty313 <chishty828rahman@gmail.com>
2024-06-14 11:44:06 +0600

    Completed till understanding error patterns in zod and mongoose

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,51 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || 'Something went wrong';
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  // Setting default Values
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'Something went wrong';
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  const errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (error instanceof ZodError) {
+    statusCode = 400;
+    message = 'Ami zod Error';
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     error,
   });
 };
 
 export default globalErrorHandler;
+
+/**
+ * Pattern =>
+ *
+ * success
+ * message
+ * errorSources: [
+ * path: '',
+ * message: ''
+ * ]
+ * stack
+ */
