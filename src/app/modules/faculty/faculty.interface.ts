61aefd2e12ec8e63703146e79d3710d70dcdf2e7
Chishty313 <chishty828rahman@gmail.com>
2024-06-16 12:24:10 +0600

    Completed till creation of interface, model, validation & constant

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,41 @@
+import { Model, Types } from 'mongoose';
+
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TGender = 'male' | 'female' | 'other';
+
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TFaculty = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImage?: string;
+  bloogGroup?: TBloodGroup;
+  academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+};
+
+export interface FacultyModel extends Model<TFaculty> {
+  isUserExists(id: string): Promise<TFaculty | null>;
+}
---
+++ src/app/modules/faculty/faculty.model.ts
@@ -0,0 +1,125 @@
+import { Schema, model } from 'mongoose';
+import { TFaculty, TUserName } from './faculty.interface';
+import { BloodGroup, Gender } from './faculty.constant';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+    maxlength: [20, 'Name can not be more than 20 characters'],
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+    trim: true,
+    maxlength: [20, 'Name can not be more than 20 characters'],
+  },
+});
+
+const facultySchema = new Schema<TFaculty>(
+  {
+    id: {
+      type: String,
+      required: [true, 'ID is required'],
+      unique: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'User ID is required'],
+      unique: true,
+      ref: 'User',
+    },
+    designation: {
+      type: String,
+      required: [true, 'Designation is required'],
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, 'Name is required'],
+    },
+    gender: {
+      type: String,
+      required: [true, 'Gender is required'],
+      enum: {
+        values: Gender,
+        message: '{VALUE} is not a valid gender',
+      },
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: [true, 'Contact number is required'],
+    },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency Contact number is required'],
+    },
+    bloogGroup: {
+      type: String,
+      enum: {
+        values: BloodGroup,
+        message: '{VALUE} is not a valid blood group',
+      },
+    },
+    presentAddress: {
+      type: String,
+      required: [true, 'Present Address is required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent Address is required'],
+    },
+    profileImage: {
+      type: String,
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'Academic department is required'],
+      ref: 'AcademicDepartment',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { toJSON: { virtuals: true } },
+);
+
+facultySchema.virtual('fullName').get(function () {
+  return `${this?.name?.firstName} ${this?.name?.middleName} ${this?.name?.lastName}`;
+});
+
+// filter out deleted documents
+facultySchema.pre('find', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+facultySchema.pre('findOne', function (next) {
+  this.findOne({ isDeleted: { $ne: true } });
+  next();
+});
+
+facultySchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
+// checking if user is already exist !!
+facultySchema.statics.isUserExists = async function (id: string) {
+  return await Faculty.findOne({ id });
+};
+
+export const Faculty = model<TFaculty>('Faculty', facultySchema);
