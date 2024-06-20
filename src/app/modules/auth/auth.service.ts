f2581f8cfec4db1062b70f91b6b6fc9c9047fcd6
Chishty313 <chishty828rahman@gmail.com>
2024-06-21 00:55:12 +0600

    Completed till creating refresh token when login and sent it to client

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,9 +2,10 @@ import httpStatus from 'http-status';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
 import bcrypt from 'bcrypt';
+import { createToken } from './auth.utils';
 
 const loginUser = async (payload: TLoginUser) => {
   const { id, password } = payload;
@@ -42,15 +43,23 @@ const loginUser = async (payload: TLoginUser) => {
     role: user.role,
   };
 
-  const accessToken = jwt.sign(
+  const accessToken = createToken(
     jwtPayload,
     config.jwt_access_secrect as string,
-    {
-      expiresIn: '10d',
-    },
+    config.jwt_access_expires_in as string,
   );
 
-  return { accessToken, needsPasswordChange: user?.needsPasswordChange };
+  const refreshToken = createToken(
+    jwtPayload,
+    config.jwt_refresh_secrect as string,
+    config.jwt_refresh_expires_in as string,
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    needsPasswordChange: user?.needsPasswordChange,
+  };
 };
 
 const changePasswordIntoDB = async (
