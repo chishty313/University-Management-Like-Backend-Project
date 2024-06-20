a4f340a04f5549f822696f335c36dc6583763b3c
Chishty313 <chishty828rahman@gmail.com>
2024-06-20 12:54:48 +0600

    Completed till creating auth middleware

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import { FacultyControllers } from './faculty.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { facultyValidations } from './faculty.validation';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.get('/', FacultyControllers.getAllFaculties);
+router.get('/', auth(), FacultyControllers.getAllFaculties);
 router.get('/:facultyId', FacultyControllers.getSingleFaculty);
 router.patch(
   '/:facultyId',
