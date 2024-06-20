6532c5d9e88dd562a36f9b352b22ea57c4d8ca04
Chishty313 <chishty828rahman@gmail.com>
2024-06-20 13:12:28 +0600

    Completed till verifying token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,14 +2,36 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
     console.log(token);
+
+    // check if the token is sent from the client
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !!!');
     }
+
+    // check if the token is valid or not
+    jwt.verify(
+      token,
+      config.jwt_access_secrect as string,
+      function (error, decoded) {
+        // error
+        if (error) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            'You are not authorized !!!',
+          );
+        }
+        // decoded
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
     next();
   });
 };
